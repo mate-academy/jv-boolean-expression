d859b08db86417cde9c9483a926748b2d0a77a3e
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-20 12:09:29 +0300

    Update BooleanExpression.java

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -13,6 +13,15 @@ public class BooleanExpression {
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return false;
+        boolean param[] = {a, b, c, d};
+        int count = 0;
+        for(int i = 0; i < 4; i++) {
+            if(param[i] == true) {
+                count++;
+            }
+        }
+        if(count == 2) {
+            return true;
+        }else return false;
     }
 }
