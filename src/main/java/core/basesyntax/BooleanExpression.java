80fb7a9d622dd0c07c5d4ea518562108cb9656aa
Ruslan <vip.russle@gmail.com>
2019-08-20 16:47:43 +0300

    Added a new boolean function for solution task 2.1. By using operator "if", "else" and and type boolean

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -11,18 +11,6 @@ public class BooleanExpression {
      * Это задание необходимо решать с использованием логических операторов.
      */
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        boolean isGood = true;
-        boolean result = true;
-        if (a == b == c || a == b == c == d) {
-            return false;
-        } else if ((isGood == a && b) || (isGood == a && c)
-            || (isGood == a && d) || (isGood == b && c)
-            || (isGood == b && d) || (isGood == c && d)) {
-            return true;
-        } else {
-            result = false;
-        }
-        return result;
 
     }
 }
