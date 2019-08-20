28803ab0aeb6b1b502b4a0e4aa7951c93a010733
Valdes2 <52889938+Valdes2@users.noreply.github.com>
2019-08-20 15:25:37 +0300

    added solution for homework BooleanExpression

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -13,25 +13,8 @@ public class BooleanExpression {
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        int m1 = 0;
-        int m2 = 0;
 
-        if (a == true && b == true) {
-            m1 = 2;
-        } else if (a == true ^ b == true) {
-            m1 = 1;
-        }
-
-        if (c == true && d == true) {
-            m2 = 2;
-        } else if (c == true ^ d == true) {
-            m2 = 1;
-        }
-
-        if (m1 + m2 == 2) {
-            return true;
-        }
-
-        return false;
+        return (a && b && !c && !d) || (a && !b && c && !d) || (a && !b && !c && d)
+                || (!a && b && c && !d) || (!a && b && !c && d) || (!a && !b && c && d);
     }
 }
