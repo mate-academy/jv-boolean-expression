d3781f5c29f9631281069259097c7ee35a52394b
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-20 13:07:10 +0300

    booleanExpression method finished in BooleanExpression class

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -6,13 +6,24 @@ public class BooleanExpression {
      * Реализуйте метод booleanExpression(boolean a, boolean b, boolean c, boolean d),
      * возвращающий true, если среди четырех его аргументов ровно два истинны (любые)
      * во всех остальных случаях метод должен возвращать false.
-     *
      * Вам надо только проанализировать переданные в метод booleanExpression
      * значения (a, b, c, d) и вернуть результат.
-     *
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return false;
+        int trueValuesQ = 0;
+        if (a == true) {
+            trueValuesQ++;
+        }
+        if (b == true) {
+            trueValuesQ++;
+        }
+        if (c == true) {
+            trueValuesQ++;
+        }
+        if (d == true) {
+            trueValuesQ++;
+        }
+        return trueValuesQ == 2;
     }
 }
