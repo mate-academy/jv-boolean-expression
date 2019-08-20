6e59ca6015418be6b17dcdcd1e4d9285a212b92d
Rosinskyi <sergey22061993@gmail.com>
2019-08-20 11:22:39 +0300

    added solution for jv-boolean-expression

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -6,13 +6,19 @@ public class BooleanExpression {
      * Реализуйте метод booleanExpression(boolean a, boolean b, boolean c, boolean d),
      * возвращающий true, если среди четырех его аргументов ровно два истинны (любые)
      * во всех остальных случаях метод должен возвращать false.
-     *
      * Вам надо только проанализировать переданные в метод booleanExpression
      * значения (a, b, c, d) и вернуть результат.
-     *
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+        if (a == true & b == true & c == false & d == false
+                || a == true & c == true & b == false & d == false
+                || a == true & d == true & b == false & c == false
+                || b == true & c == true & a == false & d == false
+                || b == true & d == true & a == false & c == false
+                || c == true & d == true & a == false & b == false) {
+            return true;
+        }
         return false;
     }
 }
