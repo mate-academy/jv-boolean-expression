f275f867a05140f5192ee6b9ce01304daf103171
Andrew <tolstuy1996@gmail.com>
2019-08-20 21:45:07 +0300

    add selection for task

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -6,13 +6,13 @@ public class BooleanExpression {
      * Реализуйте метод booleanExpression(boolean a, boolean b, boolean c, boolean d),
      * возвращающий true, если среди четырех его аргументов ровно два истинны (любые)
      * во всех остальных случаях метод должен возвращать false.
-     *
+     * <p>
      * Вам надо только проанализировать переданные в метод booleanExpression
      * значения (a, b, c, d) и вернуть результат.
-     *
+     * <p>
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return false;
+        return ((a ^ b) & (d ^ c)) | ((b ^ d) & (a ^ c)) | ((b ^ c) & (a ^ d));
     }
 }
