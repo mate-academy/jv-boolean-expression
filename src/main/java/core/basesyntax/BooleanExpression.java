ff9962418a61e2e4383d1016f3ac508e360fa705
Sviatoslav <s.granovskiy@gmail.com>
2019-08-20 16:44:32 +0300

    hw-004g-solution

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -13,6 +13,6 @@ public class BooleanExpression {
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return false;
+        return ((a ^ b) & (c ^ d)) | ((a ^ c) & (b ^ d)) | ((a ^ d) & (b ^ c));
     }
 }
