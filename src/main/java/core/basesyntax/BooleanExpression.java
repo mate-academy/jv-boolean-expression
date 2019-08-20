ce478aad2faa8cf9a7b318822ba5a21bcb040389
AndreyDMA <andreydmitriev@ukr.net>
2019-08-20 18:33:59 +0300

    solution with logic operators

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -11,24 +11,7 @@ public class BooleanExpression {
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        int count = 0;
-        if (a == true) {
-            count++;
-        }
-        if (b == true) {
-            count++;
-        }
-        if (c == true) {
-            count++;
-        }
-        if (d == true) {
-            count++;
-        }
 
-        if (count == 2) {
-            return true;
-        } else {
-            return false;
-        }
+        return (a ^ b) & (c ^ d) | (a ^ c) & (b ^ d) | (a ^ d) & (b ^ c);
     }
 }
