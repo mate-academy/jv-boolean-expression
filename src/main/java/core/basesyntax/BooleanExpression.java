ce23b21736955d8442b094a2b763c158c574ee91
Ruslan <vip.russle@gmail.com>
2019-08-20 17:26:39 +0300

    Added a new boolean function for solution task 2.1. By using operator "if", "else" and and type boolean

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -1,28 +1,16 @@
 package core.basesyntax;
 
 public class BooleanExpression {
-
     /**
-     * Реализуйте метод booleanExpression(boolean a, boolean b, boolean c, boolean d),
-     * возвращающий true, если среди четырех его аргументов ровно два истинны (любые)
-     * во всех остальных случаях метод должен возвращать false.
-     * Вам надо только проанализировать переданные в метод booleanExpression
-     * значения (a, b, c, d) и вернуть результат.
-     * Это задание необходимо решать с использованием логических операторов.
+     * Реализуйте метод booleanExpression(boolean a, boolean b, boolean c, boolean d), возвращающий
+     * true, если среди четырех его аргументов ровно два истинны (любые) во всех остальных случаях
+     * метод должен возвращать false. Вам надо только проанализировать переданные в метод
+     * booleanExpression значения (a, b, c, d) и вернуть результат. Это задание необходимо решать с
+     * использованием логических операторов.
      */
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        boolean isGood = true;
-        boolean result = true;
-        if (a == b == c || a == b == c == d) {
-            return false;
-        } else if ((isGood == a && b) || (isGood == a && c)
-            || (isGood == a && d) || (isGood == b && c)
-            || (isGood == b && d) || (isGood == c && d)) {
-            return true;
-        } else {
-            result = false;
-        }
-        return result;
-
+        return ((a && b && !c && !d) || (a && c && !b && !d)
+                || (a && d && !b && !c) || (b && c && !a && !d)
+                || (b && d && !a && !c) || (c && d && !a && !b));
     }
 }
