0748eaa6315712a1fd16d03fcb839bcbeef09369
Taranarnik <pavelkurilyuk97@gmail.com>
2019-08-20 11:42:21 +0300

    added solution for booleans checker homework

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -6,13 +6,29 @@ public class BooleanExpression {
      * Реализуйте метод booleanExpression(boolean a, boolean b, boolean c, boolean d),
      * возвращающий true, если среди четырех его аргументов ровно два истинны (любые)
      * во всех остальных случаях метод должен возвращать false.
-     *
+     *<p>
      * Вам надо только проанализировать переданные в метод booleanExpression
      * значения (a, b, c, d) и вернуть результат.
-     *
+     *</p>
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+        int counter = 0;
+        if (a) {
+            counter++;
+        }
+        if (b) {
+            counter++;
+        }
+        if (c) {
+            counter++;
+        }
+        if (d) {
+            counter++;
+        }
+        if (counter == 2) {
+            return true;
+        }
         return false;
     }
 }
