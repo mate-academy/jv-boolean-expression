60c3775ff26f8c90872e4863731a465280cf63de
Тарас <tarashryts@gmail.com>
2019-08-20 15:18:35 +0300

    add hw-2-solution-boolean-expression

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -6,21 +6,18 @@ public class BooleanExpression {
      * Реализуйте метод booleanExpression(boolean a, boolean b, boolean c, boolean d),
      * возвращающий true, если среди четырех его аргументов ровно два истинны (любые)
      * во всех остальных случаях метод должен возвращать false.
-     *
+     * <p>
      * Вам надо только проанализировать переданные в метод booleanExpression
      * значения (a, b, c, d) и вернуть результат.
-     *
+     * <p>
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        int intA = a ? 1 : 0;
-        int intB = b ? 1 : 0;
-        int intC = c ? 1 : 0;
-        int intD = d ? 1 : 0;
-        if (intA + intB + intC + intD == 2) {
-            return true;
-        } else {
-            return false;
-        }
+        return (a & b & !c & !d)
+                || (a & !b & c & !d)
+                || (!a & b & c & !d)
+                || (a & !b & !c & d)
+                || (!a & b & !c & d)
+                || (!a & !b & c & d);
     }
 }
