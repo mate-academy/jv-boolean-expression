31f0c0e3f3fe97e44aed94612e1c597c48ff8098
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-21 00:50:05 +0300

    Update BooleanExpression.java

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
 
-public class BooleanExpression {
+public class MyBooleanExpression {
     protected boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
         boolean[] array = {a && b, a && c, a && d, b && c, b && d, c && d};
-        boolean[] arrayNOT = {!(a && b), !(a && c), !(a && d), !(b && c), !(b && d), !(c && d)};
+        boolean[] arrayNegation = {!(a && b), !(a && c), !(a && d), !(b && c), !(b && d), !(c && d)};
         boolean flag = true;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] && arrayNOT[i] == true) {
+            if (array[i] && arrayNegation[i] == true) {
                 flag = false;
             }
         }
