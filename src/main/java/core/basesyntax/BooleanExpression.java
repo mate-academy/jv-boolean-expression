6809f1952bef82cdb2e7619e8faa040480108bc6
Ruslan <vip.russle@gmail.com>
2019-08-20 15:38:40 +0300

    Added a new boolean function for solution task 2.1. By using operator "if", "else" and and type boolean

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -6,13 +6,26 @@ public class BooleanExpression {
      * Реализуйте метод booleanExpression(boolean a, boolean b, boolean c, boolean d),
      * возвращающий true, если среди четырех его аргументов ровно два истинны (любые)
      * во всех остальных случаях метод должен возвращать false.
-     *
      * Вам надо только проанализировать переданные в метод booleanExpression
      * значения (a, b, c, d) и вернуть результат.
      *
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return false;
+        boolean[] fillGood = {a, b, c, d};
+        boolean isGood = true;
+        int count = 0;
+
+        for (int j = 0; j < 4; j++) {
+            isGood = fillGood[j];
+            if (isGood == true) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
