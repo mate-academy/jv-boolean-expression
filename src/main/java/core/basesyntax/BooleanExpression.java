6fc6b7d37ea6e03f9309b6778f902e9337bda97c
Max <skaterweather41@gmail.com>
2019-08-21 11:13:13 +0200

    solution simlified

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -10,11 +10,6 @@ public class BooleanExpression {
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return     a && b && !c && !d
-                || a && c && !b && !d
-                || a && d && !b && !c
-                || b && c && !a && !d
-                || b && d && !a && !c
-                || c && d && !a && !b;
+        return (a ^ b) && (c ^ d) || (a ^ c) && (b ^ d) || (a ^ d) && (b ^ c);
     }
 }
