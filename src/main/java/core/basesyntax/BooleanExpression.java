5f3c8cd01889112ebda36eecbe3a47f4c8c29eff
roman_minevich <283389dv3qsh>
2019-08-21 12:28:41 +0300

    Change solution

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -11,13 +11,8 @@ public class BooleanExpression {
      * Это задание необходимо решать с использованием логических операторов.
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        int result = 0;
-        boolean[] abcd = {a, b, c, d};
-        for (boolean x : abcd) {
-            if (x == true) {
-                result++;
-            }
-        }
-        return result == 2;
+        return ((((a && b) ^ (c && d)) ||
+                ((a && c) ^ (b && d))) ^
+                ((a && d) ^ (b && c)));
     }
 }
