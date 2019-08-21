3bf8878fe71bd3f8a27c1b1040f7df6ce4890fa4
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-21 12:59:57 +0300

    Update BooleanExpression.java

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -1,18 +1,13 @@
 package core.basesyntax;
 
 public class BooleanExpression {
+    
     protected boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        boolean[] array = {a && b, a && c, a && d, b && c, b && d, c && d};
-        int count = 0;
-        for (boolean i : array) {
-            if (i == true) {
-                count++;
-            }
-        }
-        if (count == 1) {
-            return true;
-        } else {
-            return false;
-        }
+        return (a == b & c == d & a != d) ||
+                (a == c & b == d & a != b) ||
+                (a == d & b == c & a != b) ||
+                (b == c & a == d & b != a) ||
+                (b == d & a == c & b != a) ||
+                (c == d & a == b & c != a);
     }
 }
