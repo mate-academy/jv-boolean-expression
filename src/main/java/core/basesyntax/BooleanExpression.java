bb418fab0b1908322a20ca88721edfca26550fb0
Тарас <tarashryts@gmail.com>
2019-08-21 14:26:01 +0300

    add hw-2-solution-boolean-expression

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -12,7 +12,6 @@ public class BooleanExpression {
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
         return ((a ^ b) & (c ^ d))
-                || ((a ^ c) & (b ^ d))
-                || ((a ^ d) & (c ^ b));
+                || ((a ^ c) & (b ^ d));
     }
 }
