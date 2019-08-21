b6174b494300db796e0e104c745045a7224002ec
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-21 13:04:07 +0300

    Update BooleanExpression.java

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -1,13 +1,13 @@
 package core.basesyntax;
 
 public class BooleanExpression {
-    
+
     protected boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return (a == b & c == d & a != d) ||
-                (a == c & b == d & a != b) ||
-                (a == d & b == c & a != b) ||
-                (b == c & a == d & b != a) ||
-                (b == d & a == c & b != a) ||
-                (c == d & a == b & c != a);
+        return (a == b & c == d & a != d)
+                || (a == c & b == d & a != b)
+                || (a == d & b == c & a != b)
+                || (b == c & a == d & b != a)
+                || (b == d & a == c & b != a)
+                || (c == d & a == b & c != a);
     }
 }
