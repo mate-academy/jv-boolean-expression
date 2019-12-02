a233406f5c9ddc88d87158edbcc1aff539f25cc7
Stanislav Karev <karsv.dvz@gmail.com>
2019-12-02 14:30:22 +0200

    hw1.2 change for only boolean operators

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -13,13 +13,6 @@ public class BooleanExpression {
      * <p>Это задание необходимо решать с использованием логических операторов.</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        int count = 0;
-        if (a){count++;}
-        if(b){count++;}
-        if(c){count++;}
-        if(d){count++;}
-
-
-        return count == 2;
+        return a ^ b && c ^ d || a ^ c && b ^ d;
     }
 }
