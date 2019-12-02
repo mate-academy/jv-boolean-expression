7cbbca8ecb38e3cc8313ee7587d320b8f65d4722
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2019-12-02 14:57:20 +0200

    Added solution for hw 1.2

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -13,6 +13,13 @@ public class BooleanExpression {
      * <p>Это задание необходимо решать с использованием логических операторов.</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+        if (a) {
+            return (b && !c && !d) || (c && !b && !d) || (d && !b && !c);
+        } else if (b) {
+            return (c && !d) || (d && !c);
+        } else if (c) {
+            return d;
+        }
         return false;
     }
 }
