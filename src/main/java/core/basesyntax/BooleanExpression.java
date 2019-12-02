c4692150464c828180394aa17a3f9a5f2a1689fe
Bohdan Chupika <boroda4436@users.noreply.github.com>
2019-12-02 13:05:09 +0200

    Update BooleanExpression.java

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -13,6 +13,6 @@ public class BooleanExpression {
      * <p>Это задание необходимо решать с использованием логических операторов.</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return false;
+        return true;
     }
 }
