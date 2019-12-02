f0d6682510ea9f59128a53c0966d8439e25411f1
Serhii Yevtushok <ggazila@gmail.com>
2019-12-02 17:48:52 +0200

    removed empty line

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -14,6 +14,5 @@ public class BooleanExpression {
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
         return a ^ b && c ^ d || a ^ c && b ^ d;
-
     }
 }
