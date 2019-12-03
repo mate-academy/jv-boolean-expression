6d6042ec587070700711ad79d2a41f98cfdcce0f
MrNopi <Smart.like1998@gmail.com>
2019-12-03 12:22:11 +0300

    clean some code, all tests had been passed

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -13,10 +13,6 @@ public class BooleanExpression {
      * <p>Это задание необходимо решать с использованием логических операторов.</p>
      */
 
-    public static void main(String[] args) {
-        System.out.println(booleanExpression(true, true, false, false));
-    }
-
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
         return ((a ^ b) && c ^ d) || (b ^ c && a ^ d);
     }
