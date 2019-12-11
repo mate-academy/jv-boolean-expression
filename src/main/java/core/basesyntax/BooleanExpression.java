3f9e2ca493ac9d5d28a0d47e8a89440a9db81977
Sofasmile <sofiya.dyakonova@ukr.net>
2019-12-11 10:41:59 +0200

    changed description

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -10,7 +10,8 @@ public class BooleanExpression {
      * <p>Вам надо только проанализировать переданные в метод booleanExpression
      * значения (a, b, c, d) и вернуть результат.</p>
      *
-     * <p>Это задание необходимо решать с использованием логических операторов.</p>
+     * <p>Это задание необходимо решать с использованием логических операторов,
+     * без использования конструкций if-else</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
         return false;
