d61886a68e5f96d3fe74f5335896d9af4fbc8929
NastiaFasova <fasovaaa@fmail.com>
2020-03-30 16:43:05 +0300

    HomeWork-1(task2) Edited the solution, used binary operators to simplify

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -14,7 +14,6 @@ public class BooleanExpression {
      * без использования конструкций if-else</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return (a && b || a && c || a && d || b && d || c && d || b && c)
-                && (!a && !b || !a && !c || !a && !d || !b && !d || !c && !d || !b && !c);
+        return ((a ^ b) && (c ^ d) || (a ^ c) && (b ^ d));
     }
 }
