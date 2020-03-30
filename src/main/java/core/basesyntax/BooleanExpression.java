346d3f963a6dd192c543b96a56d61bcacfea8442
vladpetrovskyi <v.petrovskyi98@gmail.com>
2020-03-30 15:58:28 +0300

    added solution for homework 1.2

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -14,6 +14,7 @@ public class BooleanExpression {
      * без использования конструкций if-else</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return false;
+        return (((a == b) && (b != c) && (c == d)) || ((a == c) && (b == d) && (b != c))
+                || ((a == d) && (b == c) && (b != a)));
     }
 }
