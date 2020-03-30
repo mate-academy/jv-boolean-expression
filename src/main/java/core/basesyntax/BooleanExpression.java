d2946bb304c4a4b5a258ef51d1e3a9d6030cee54
Andrey Tanchak <tanchak12@gmail.com>
2020-03-30 20:09:36 +0300

    added solution

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -3,19 +3,7 @@ package core.basesyntax;
 public class BooleanExpression {
 
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        int count = 0;
-        count = a == b
-                ? count + 1 : count;
-        count = a == c
-                ? count + 1 : count;
-        count = a == d
-                ? count + 1 : count;
-        count = b == c
-                ? count + 1 : count;
-        count = b == d
-                ? count + 1 : count;
-        count = c == d
-                ? count + 1 : count;
-        return count == 2;
+        return (a ^ b && c ^ d) || (a ^ c && b ^ d);
+
     }
 }
