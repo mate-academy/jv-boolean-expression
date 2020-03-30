61158ffecb0d13bd158ef8dfd33a75e02f7e33ed
unknown <bavikaalexey@gmail.com>
2020-03-30 15:40:34 +0300

    added solution

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -14,6 +14,8 @@ public class BooleanExpression {
      * без использования конструкций if-else</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return false;
+        return (a && b && !c && !d) || (a && !b && c && !d)
+                || (a && !b && !c && d) || (!a && b && c && !d)
+                || (!a && b && !c && d) || (!a && !b && c && d);
     }
 }
