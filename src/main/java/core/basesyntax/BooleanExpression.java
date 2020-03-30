ecebeea64fda532c4f7d4df52f1d851495cd360d
NastiaFasova <fasovaaa@fmail.com>
2020-03-30 14:19:25 +0300

    HomeWork-1(task2)

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -1,19 +1,8 @@
 package core.basesyntax;
 
 public class BooleanExpression {
-
-    /**
-     * <p>Реализуйте метод booleanExpression(boolean a, boolean b, boolean c, boolean d),
-     * возвращающий true, если среди четырех его аргументов ровно два истинны (любые)
-     * во всех остальных случаях метод должен возвращать false.</p>
-     *
-     * <p>Вам надо только проанализировать переданные в метод booleanExpression
-     * значения (a, b, c, d) и вернуть результат.</p>
-     *
-     * <p>Это задание необходимо решать с использованием логических операторов,
-     * без использования конструкций if-else</p>
-     */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return false;
+        return (a && b || a && c || a && d || b && d || c && d || b && c)
+                && (!a && !b || !a && !c || !a && !d || !b && !d || !c && !d || !b && !c);
     }
 }
