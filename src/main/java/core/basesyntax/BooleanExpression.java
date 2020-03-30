dbb7c42b91b4f6e58194c0e2d767704686f2f920
6ALLIKA <skorik009@gmail.com>
2020-03-30 15:32:34 +0300

    added solution with binary operators

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -14,7 +14,7 @@ public class BooleanExpression {
      * без использования конструкций if-else</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return (a && b || a && c || a && d || b && c || b && d || c && d)
-                && (!a && !b || !a && !c || !a && !d || !b && !c || !b && !d || !c && !d);
+        return (a ^ b && c ^ d)
+                || (a ^ c && b ^ d);
     }
 }
