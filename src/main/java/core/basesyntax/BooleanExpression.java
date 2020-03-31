febea144678b9e90cdea983b963ee7f52c31c6a6
VovkAA <vovkandrew@gmail.com>
2020-03-31 12:50:19 +0300

    Simplified again with Pavel's help

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -15,6 +15,6 @@ public class BooleanExpression {
      */
 
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return ((a ^ b) & (c ^ d)) | ((a ^ c) & (b ^ d)) | ((a ^ d) & (c ^ b));
+        return ((a ^ b) & (c ^ d)) | ((a ^ c) & (b ^ d));
     }
 }
