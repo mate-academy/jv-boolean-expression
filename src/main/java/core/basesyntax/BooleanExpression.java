072dcfeac6b03630b9d0516ac9f3a1c3bf006dd3
Ilya.Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2020-07-27 17:40:54 +0300

    fixed with boolean operators

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -14,7 +14,6 @@ public class BooleanExpression {
      * без использования конструкций if-else</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return (a ? 1 : 0) + (b ? 1 : 0) + (c ? 1 : 0) + (d ? 1 : 0) == 2;
-
+        return (a ^ c && b ^ d) || (a ^ b && c ^ d);
     }
 }
