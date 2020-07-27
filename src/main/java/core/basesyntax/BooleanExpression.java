0f4caab492b9497475f19f5eea0d5f31b7435143
Artem Vremere <vremere_a@ukr.net>
2020-07-27 17:17:37 +0300

    changed solution. used && and ||

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -14,7 +14,6 @@ public class BooleanExpression {
      * без использования конструкций if-else</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return (a & b) & !c & !d | (a & c) & !b & !d | (a & d) & !c & !b | (c & d) & !a & !b
-                | (b & d) & !a & !c | (b & c) & !a & !d;
+        return a ^ b && c ^ d || a ^ c && b ^ d;
     }
 }
