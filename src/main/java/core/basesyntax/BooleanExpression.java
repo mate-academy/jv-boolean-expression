0e05d34ccf006b191d94777ec042db95e18e57d7
Sashk52 <a.tsven@gmail.com>
2020-12-26 20:34:05 +0200

    just commit to check work.please ignore it

--- src/main/java/core/basesyntax/BooleanExpression.java
+++ src/main/java/core/basesyntax/BooleanExpression.java
@@ -14,6 +14,19 @@ public class BooleanExpression {
      * без использования конструкций if-else</p>
      */
     public boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return false;
+        int count = 0;
+        if (a == true) {
+            count++;
+        }
+        if (b == true) {
+            count++;
+        }
+        if (c == true) {
+            count++;
+        }
+        if (d == true) {
+            count++;
+        }
+        return count == 2;
     }
 }
